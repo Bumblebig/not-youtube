255e71ad46f924603881f68e40aaaf436642d84b
Abdurrahman <dreydare16@gmail.com>
2023-11-26 12:29:22 +0100

    generated region code from browser

--- src/components/VideosPage.jsx
+++ src/components/VideosPage.jsx
@@ -15,7 +15,7 @@ export default function VideoPage() {
 
 
 //   Get region code
-const userLanguage = navigator.language;
+const userLanguage = navigator.locale;
 const region = userLanguage.split('-')[1];
 
 
